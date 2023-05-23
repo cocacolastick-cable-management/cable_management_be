6997268e86b7db558de28fd9d7216dd608fdd819
Say my name <vuphamlethanh.public@gmail.com>
2023-05-23 20:45:47 +0700

    feat<infra>: create `DB` instance

---
+++ src/infra/database/db.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"github.com/cable_management/cable_management_be/config"
+	"github.com/cable_management/cable_management_be/src/entities"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+var (
+	DB *gorm.DB
+)
+
+func init() {
+
+	dsn := config.Env.DbDsn
+
+	var err error = nil
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	err = DB.AutoMigrate(&entities.Admin{}, &entities.Supplier{}, &entities.Planner{}, &entities.Contractor{})
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+}
