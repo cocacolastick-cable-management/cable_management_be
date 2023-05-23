576cc9080f797123b0501291980533850301bc0a
Say my name <vuphamlethanh.public@gmail.com>
2023-05-23 21:49:21 +0700

    feat<service>: create `NewPasswordHashService`

--- src/services/password_hash_service.go
+++ src/services/password_hash_service.go
@@ -9,6 +9,10 @@ type IPasswordHashService interface {
 type PasswordHashService struct {
 }
 
+func NewPasswordHashService() *PasswordHashService {
+	return &PasswordHashService{}
+}
+
 func (phs PasswordHashService) Hash(password string) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
