7ac6845a108c3afe462bcf266d4c7ab658de8b5f
Say my name <vuphamlethanh.public@gmail.com>
2023-05-24 00:44:10 +0700

    feat<services>: create `Compare` func for `PasswordHashService`

--- src/services/password_hash_service.go
+++ src/services/password_hash_service.go
@@ -4,6 +4,7 @@ import "golang.org/x/crypto/bcrypt"
 
 type IPasswordHashService interface {
 	Hash(password string) (string, error)
+	Compare(passwordHash, password string) bool
 }
 
 type PasswordHashService struct {
@@ -19,4 +20,9 @@ func (phs PasswordHashService) Hash(password string) (string, error) {
 		return "", err
 	}
 	return string(hashedPassword), nil
+}
+
+func (phs PasswordHashService) Compare(passwordHash, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(password))
+	return err == nil
 }
