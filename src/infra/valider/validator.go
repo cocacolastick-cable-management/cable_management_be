114b7b6d8f68aa7587e48eb2e28bed4be38f4f22
Say my name <vuphamlethanh.public@gmail.com>
2023-05-24 17:19:16 +0700

    refactor: move all custom validation to `usecases`

--- src/infra/valider/validator.go
+++ src/infra/valider/validator.go
@@ -1,9 +1,9 @@
 package valider
 
 import (
-	"github.com/cable_management/cable_management_be/src/services"
+	"github.com/cable_management/cable_management_be/src/usecases/adminUsecases"
+	"github.com/cable_management/cable_management_be/src/usecases/commUsecases"
 	"github.com/go-playground/validator/v10"
-	"golang.org/x/exp/slices"
 )
 
 var (
@@ -13,21 +13,18 @@ var (
 func init() {
 	Validator = validator.New()
 
-	err := Validator.RegisterValidation("validateRole", validateRole)
+	err := Validator.RegisterValidation("validateRole", commUsecases.ValidateRole)
 	if err != nil {
 		panic(err)
 	}
-}
 
-func validateRole(fl validator.FieldLevel) bool {
+	err = Validator.RegisterValidation("validateRoleExceptAdmin", adminUsecases.ValidateRoleExceptAdmin)
+	if err != nil {
+		panic(err)
+	}
 
-	roles := []string{
-		services.AdminRole,
-		services.PlannerRole,
-		services.SupplierRole,
-		services.ContractorRole}
-
-	value := fl.Field().Interface().(string)
-
-	return slices.Contains[string](roles, value)
+	err = Validator.RegisterValidation("validatePassword", adminUsecases.ValidatePassword)
+	if err != nil {
+		panic(err)
+	}
 }
--- src/usecases/commUsecases/sign_in.go
+++ src/usecases/commUsecases/sign_in.go
@@ -3,6 +3,7 @@ package commUsecases
 import (
 	"github.com/cable_management/cable_management_be/src/services"
 	"github.com/go-playground/validator/v10"
+	"golang.org/x/exp/slices"
 )
 
 type SignInRequestDto struct {
@@ -23,7 +24,7 @@ type SignIn struct {
 func (si SignIn) Handle(signInRequest SignInRequestDto) (*services.AuthData, error) {
 
 	var err error
-	
+
 	err = si.validator.Struct(signInRequest)
 	if err != nil {
 		return nil, err
@@ -36,4 +37,17 @@ func (si SignIn) Handle(signInRequest SignInRequestDto) (*services.AuthData, err
 		signInRequest.Password)
 
 	return authData, err
+}
+
+func ValidateRole(fl validator.FieldLevel) bool {
+
+	roles := []string{
+		services.AdminRole,
+		services.PlannerRole,
+		services.SupplierRole,
+		services.ContractorRole}
+
+	value := fl.Field().Interface().(string)
+
+	return slices.Contains[string](roles, value)
 }
