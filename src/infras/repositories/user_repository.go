ca4c2d319336dfb0b7dae43de4cff2567cfb5820
Say my name <vuphamlethanh.public@gmail.com>
2023-05-26 10:36:05 +0700

    feat<repositories>: create `UserRepository`

---
+++ src/infras/repositories/user_repository.go
@@ -0,0 +1,29 @@
+package repositories
+
+import (
+	"github.com/cable_management/cable_management_be/src/entities"
+	"gorm.io/gorm"
+)
+
+type IUserRepository interface {
+	FindByEmail(email string) (*entities.User, error)
+}
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (ur UserRepository) FindByEmail(email string) (*entities.User, error) {
+
+	matchingUser := &entities.User{}
+	result := ur.db.First(matchingUser, "email = ?", email)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return matchingUser, nil
+}
