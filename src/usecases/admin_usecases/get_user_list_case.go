cd6aa2d75ecb7481a2f979244d1e13c1d0368495
Say my name <vuphamlethanh.public@gmail.com>
2023-05-26 21:28:49 +0700

    feat<usecases>: create `GetUserListCase`

---
+++ src/usecases/admin_usecases/get_user_list_case.go
@@ -0,0 +1,52 @@
+package admin_usecases
+
+import (
+	"github.com/cable_management/cable_management_be/infras/db/repositories"
+	"github.com/cable_management/cable_management_be/src/constants"
+	"github.com/cable_management/cable_management_be/src/errs"
+	"github.com/cable_management/cable_management_be/src/services"
+	"github.com/cable_management/cable_management_be/src/usecases/_commons/requests"
+	"github.com/cable_management/cable_management_be/src/usecases/_commons/responses"
+	"github.com/go-playground/validator/v10"
+)
+
+type IGetUserListCase interface {
+	Handle(accessToken string, request *requests.PaginationRequest) ([]responses.UserResponse, error)
+}
+
+type GetUserListCase struct {
+	tokenService services.IAuthTokenService
+	validator    *validator.Validate
+	userRepo     repositories.IUserRepository
+}
+
+func (gul GetUserListCase) Handle(accessToken string, request requests.PaginationRequest) ([]*responses.UserResponse, error) {
+
+	isTokenValid, claims := gul.tokenService.IsAccessTokenValid(accessToken)
+	if !isTokenValid {
+		return nil, errs.ErrAuthFailed
+	}
+
+	if claims.Role != constants.AdminRole {
+		return nil, errs.ErrUnAuthorized
+	}
+
+	err := gul.validator.Struct(request)
+	if err != nil {
+		return nil, err
+	}
+
+	userList, _ := gul.userRepo.GetList(request.Page, request.Size, request.LastTimestamp)
+
+	response := make([]*responses.UserResponse, len(userList))
+	for i, user := range userList {
+		response[i] = &responses.UserResponse{
+			Id:        user.Id,
+			Role:      user.Role,
+			Email:     user.Email,
+			CreatedAt: user.CreatedAt,
+		}
+	}
+
+	return response, nil
+}
