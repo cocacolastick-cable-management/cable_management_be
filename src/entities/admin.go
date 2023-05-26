07e274919af22088bd90a807895a0e7335d90ed5
Say my name <vuphamlethanh.public@gmail.com>
2023-05-26 10:10:50 +0700

    feat<entities>: create `Admin`

---
+++ src/entities/admin.go
@@ -0,0 +1,12 @@
+package entities
+
+type Admin struct {
+	AbstractEntity
+
+	Email        string `gorm:"unique;type:varchar"`
+	PasswordHash string `gorm:"unique;type:varchar"`
+}
+
+func NewAdmin(email string, passwordHash string) *Admin {
+	return &Admin{AbstractEntity: NewAbstractEntity(), Email: email, PasswordHash: passwordHash}
+}
