52930549f313e9b85b3cf78bddc7ebfcf477bf0d
Say my name <vuphamlethanh.public@gmail.com>
2023-05-26 21:26:34 +0700

    Update pagination_request.go

--- src/usecases/_commons/requests/pagination_request.go
+++ src/usecases/_commons/requests/pagination_request.go
@@ -3,7 +3,7 @@ package requests
 import "time"
 
 type PaginationRequest struct {
-	Page          int16      `validate:"required;min=0"`
-	Size          int8       `validate:"required;min=0;max=225"`
+	Page          uint
+	Size          uint       `validate:"max=225"`
 	LastTimestamp *time.Time `validate:"omitempty"`
 }
