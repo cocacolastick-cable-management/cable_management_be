97b1add0493ab2524dbd5911f7ae15353d5027b2
Say my name <vuphamlethanh.public@gmail.com>
2023-05-27 22:06:30 +0700

    feat<requests>: add `OrderBy` for `PaginationRequest`

--- src/features/dtos/requests/pagination_request.go
+++ src/features/dtos/requests/pagination_request.go
@@ -3,7 +3,8 @@ package requests
 import "time"
 
 type PaginationRequest struct {
-	Page          uint
-	Size          uint       `validate:"max=225"`
-	LastTimestamp *time.Time `validate:"omitempty"`
+	Page          uint       `json:"page,omitempty" validate:"required"`
+	Size          uint       `json:"size,omitempty" validate:"required,max=225"`
+	OrderBy       *string    `json:"orderBy,omitempty" validate:"omitempty"`
+	LastTimestamp *time.Time `json:"lastTimestamp,omitempty" validate:"omitempty"`
 }
