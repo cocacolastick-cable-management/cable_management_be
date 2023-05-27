c30fcc40269972be0615e736bef5023dd4a26a50
Say my name <vuphamlethanh.public@gmail.com>
2023-05-27 22:03:26 +0700

    feat<utils>: create `ValidationErrorResponse`

--- src/app/utils/common_response_data.go
+++ src/app/utils/common_response_data.go
@@ -1,6 +1,9 @@
 package utils
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+)
 
 func UnAuthenticatedResponse(ctx *fiber.Ctx) error {
 	return ctx.Status(401).JSON(Response{
@@ -13,5 +16,13 @@ func UnAuthorizedResponse(ctx *fiber.Ctx) error {
 	return ctx.Status(403).JSON(Response{
 		Message: "unauthorized",
 		Code:    "UA",
+	})
+}
+
+func ValidationErrorResponse(ctx *fiber.Ctx, validError validator.ValidationErrors) error {
+	return ctx.Status(400).JSON(Response{
+		Message: "invalidation",
+		Code:    "IVL",
+		Errors:  ValidationErrorToStruct(validError),
 	})
 }
