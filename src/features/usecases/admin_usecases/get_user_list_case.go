1ceca86df2e57eeb51c1f280aa6aa5c830485e47
Say my name <vuphamlethanh.public@gmail.com>
2023-05-28 02:01:35 +0700

    Update get_user_list_case.go

--- src/features/usecases/admin_usecases/get_user_list_case.go
+++ src/features/usecases/admin_usecases/get_user_list_case.go
@@ -3,7 +3,6 @@ package admin_usecases
 import (
 	"github.com/cable_management/cable_management_be/src/domain/constants"
 	"github.com/cable_management/cable_management_be/src/domain/contracts/db/repositories"
-	"github.com/cable_management/cable_management_be/src/domain/services"
 	"github.com/cable_management/cable_management_be/src/features/dtos/requests"
 	"github.com/cable_management/cable_management_be/src/features/dtos/responses"
 	"github.com/cable_management/cable_management_be/src/features/helpers"
@@ -15,16 +14,14 @@ type IGetUserListCase interface {
 }
 
 type GetUserListCase struct {
-	tokenService       services.IAuthTokenService
 	validator          *validator.Validate
 	userRepo           repositories.IUserRepository
 	makeSureAuthorized helpers.IMakeSureAuthorized
 }
 
-func NewGetUserListCase(tokenService services.IAuthTokenService, validator *validator.Validate, userRepo repositories.IUserRepository, makeSureAuthorized helpers.IMakeSureAuthorized) *GetUserListCase {
-	return &GetUserListCase{tokenService: tokenService, validator: validator, userRepo: userRepo, makeSureAuthorized: makeSureAuthorized}
+func NewGetUserListCase(validator *validator.Validate, userRepo repositories.IUserRepository, makeSureAuthorized helpers.IMakeSureAuthorized) *GetUserListCase {
+	return &GetUserListCase{validator: validator, userRepo: userRepo, makeSureAuthorized: makeSureAuthorized}
 }
-
 func (gul GetUserListCase) Handle(accessToken string, request requests.PaginationRequest) ([]*responses.UserResponse, error) {
 
 	_, err := gul.makeSureAuthorized.Handle(accessToken, constants.AdminRole)
