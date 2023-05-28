4663e3a4a181f95018ef238960acbfb10827ffba
Say my name <vuphamlethanh.public@gmail.com>
2023-05-29 00:47:00 +0700

    feat<helpers>: create `MapHistoryResponse`

---
+++ src/features/helpers/map_history_response.go
@@ -0,0 +1,24 @@
+package helpers
+
+import (
+	"github.com/cable_management/cable_management_be/src/domain/entities"
+	"github.com/cable_management/cable_management_be/src/domain/errs"
+	"github.com/cable_management/cable_management_be/src/features/dtos/responses"
+)
+
+func MapHistoryResponse(history *entities.WithDrawRequestHistory) (*responses.HistoryResponse, error) {
+
+	if history.Creator == nil {
+		return nil, errs.ErrNotIncludeRelationship
+	}
+
+	return &responses.HistoryResponse{
+		CreatorId:    history.CreatorId,
+		CreatorName:  history.Creator.DisplayName,
+		CreatorEmail: history.Creator.Email,
+		CreatorRole:  history.Creator.Role,
+		CreatedAt:    history.CreatedAt.UTC(),
+		Status:       history.Status,
+		Action:       history.Action,
+	}, nil
+}
