ae684f349453ac14a616413df31b8494c364501d
Say my name <vuphamlethanh.public@gmail.com>
2023-06-02 17:45:46 +0700

    fix<responses>: update `ContractResponse`

--- src/features/dtos/responses/contract_response.go
+++ src/features/dtos/responses/contract_response.go
@@ -6,12 +6,14 @@ import (
 )
 
 type ContractResponse struct {
-	Id           uuid.UUID
-	SupplierId   uuid.UUID
-	SupplierName string
-	CableAmount  uint
-	Stock        int
-	StartDay     time.Time
-	EndDay       time.Time
-	CreatedAt    time.Time
+	Id            uuid.UUID
+	UniqueName    string
+	SupplierId    uuid.UUID
+	SupplierName  string
+	SupplierEmail string
+	CableAmount   uint
+	Stock         int
+	StartDay      time.Time
+	EndDay        time.Time
+	CreatedAt     time.Time
 }
