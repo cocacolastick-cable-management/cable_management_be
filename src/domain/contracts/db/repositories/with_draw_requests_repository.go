0a21f93c9ce29a0d13ff0c6fe0a362659fabbbd6
Say my name <vuphamlethanh.public@gmail.com>
2023-06-02 17:44:00 +0700

    Update with_draw_requests_repository.go

--- src/domain/contracts/db/repositories/with_draw_requests_repository.go
+++ src/domain/contracts/db/repositories/with_draw_requests_repository.go
@@ -3,11 +3,10 @@ package repositories
 import (
 	"github.com/cable_management/cable_management_be/src/domain/entities"
 	"github.com/google/uuid"
-	"time"
 )
 
 type IWithDrawRequestRepository interface {
 	Insert(withDrawRequest *entities.WithDrawRequest) error
 	FindById(id uuid.UUID, withs []string) (*entities.WithDrawRequest, error)
-	GetList(page uint, size uint, orderBy *string, lastTimestamp *time.Time, withs []string) ([]*entities.WithDrawRequest, error)
+	GetList(withs []string) ([]*entities.WithDrawRequest, error)
 }
