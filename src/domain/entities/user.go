149257e2ff580a331cac8b5a2c4522c2a8bf12a5
Say my name <vuphamlethanh.public@gmail.com>
2023-06-02 17:48:38 +0700

    Update user.go

--- src/domain/entities/user.go
+++ src/domain/entities/user.go
@@ -11,9 +11,10 @@ type User struct {
 	Email        string `gorm:"uniqueIndex:uni_user;type:varchar"`
 	PasswordHash string `gorm:"type:varchar"`
 	Role         string `gorm:"uniqueIndex:uni_user;type:varchar"`
+	IsActive     bool   `gorm:"type:boolean"`
 	CreatedAt    time.Time
 }
 
-func NewUser(role, displayName, email, passwordHash string, createdAt time.Time) *User {
-	return &User{AbstractEntity: NewAbstractEntity(), Role: role, DisplayName: displayName, Email: email, PasswordHash: passwordHash, CreatedAt: createdAt}
+func NewUser(role, displayName, email, passwordHash string, isActive bool, createdAt time.Time) *User {
+	return &User{AbstractEntity: NewAbstractEntity(), Role: role, DisplayName: displayName, Email: email, IsActive: isActive, PasswordHash: passwordHash, CreatedAt: createdAt}
 }
