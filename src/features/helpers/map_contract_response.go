72b7f1f86019a17120bec976fb27755fbdbb895a
Say my name <vuphamlethanh.public@gmail.com>
2023-06-02 17:44:49 +0700

    Create map_contract_response.go

---
+++ src/features/helpers/map_contract_response.go
@@ -0,0 +1,34 @@
+package helpers
+
+import (
+	"github.com/cable_management/cable_management_be/src/domain/entities"
+	"github.com/cable_management/cable_management_be/src/domain/errs"
+	"github.com/cable_management/cable_management_be/src/features/dtos/responses"
+)
+
+func MapContractResponse(contract *entities.Contract) (*responses.ContractResponse, error) {
+
+	stock, err := contract.CalCableStock()
+	if err != nil {
+		return nil, err
+	}
+
+	if contract.Supplier == nil {
+		return nil, errs.ErrNotIncludeRelationship
+	}
+
+	response := &responses.ContractResponse{
+		Id:            contract.Id,
+		SupplierId:    contract.SupplierId,
+		SupplierName:  contract.Supplier.DisplayName,
+		CableAmount:   contract.CableAmount,
+		Stock:         stock,
+		UniqueName:    contract.UniqueName,
+		SupplierEmail: contract.Supplier.Email,
+		StartDay:      contract.StartDay.UTC(),
+		EndDay:        contract.EndDay.UTC(),
+		CreatedAt:     contract.CreatedAt.UTC(),
+	}
+
+	return response, nil
+}
